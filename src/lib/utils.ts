734d41323159ff1c8c0798840c86a770394e3904
Thamsanqa Dreem <thamsanqa.dev@gmail.com>
2024-10-05 13:35:06 +0200

    fixed youtube link issues

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,7 +27,9 @@ export const getPlatformFromLink = (link: string) => {
   if (link.includes("instagram.com")) return "instagram";
   if (link.includes("whatsapp.com")) return "whatsapp";
   if (link.includes("tiktok.com")) return "tiktok";
-  if (link.includes("youtube.com")) return "youtube";
+  if (link.includes("youtube.com") || link.includes("youtu.be")) {
+    return "youtube";
+  }
   if (link.includes("spotify.com")) return "spotify";
   return null;
 };
